8695d64c2a0ed204cd5638ce10c78700015800f2
lihazinoob <lihazi2007038@stud.kuet.ac.bd>
2024-02-17 21:10:35 +0600

    Calculator using asp.net web forms

---
+++ Calculator/Calculator/WebForm1.aspx
@@ -0,0 +1,33 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Calculator.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            Enter a number: 
+
+        <input id="Text1" type="text" runat ="server"/></div>
+        <div id ="another">
+            Enter Another Number: 
+        <input id="Text2" type="text" runat ="server"/></div>
+        <div>
+            <asp:Button ID="Add" runat="server" Text="+" OnClick="Add_Click" />
+            <asp:Button ID="Minus" runat="server" Text="-" OnClick="Minus_Click" />
+            <asp:Button ID="Mul" runat="server" Text="*" OnClick="Mul_Click" />
+            <asp:Button ID="Div" runat="server" Text="/" OnClick="Div_Click" />
+        </div>
+        <p>
+            
+            <asp:Button ID="Clear" runat="server" Text="Clear" OnClick="Clear_Click" />
+        </p>
+        <p>
+            <asp:Label ID="Label1" runat="server" Text=""></asp:Label>
+        </p>
+    </form>
+</body>
+</html>
---
+++ Calculator/Calculator/WebForm1.aspx.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Calculator
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+     
+
+        protected void Add_Click(object sender, EventArgs e)
+        {
+            string firstnumber = Text1.Value;
+            string secondnumber = Text2.Value;
+            int num1 = Convert.ToInt32(firstnumber);
+            int num2 = Convert.ToInt32(secondnumber);
+            Label1.Text = Convert.ToString(num1 + num2);
+
+        }
+
+        protected void Minus_Click(object sender, EventArgs e)
+        {
+            string firstnumber = Text1.Value;
+            string secondnumber = Text2.Value;
+            int num1 = Convert.ToInt32(firstnumber);
+            int num2 = Convert.ToInt32(secondnumber);
+            Label1.Text = Convert.ToString(num1 - num2);
+        }
+
+        protected void Mul_Click(object sender, EventArgs e)
+        {
+            string firstnumber = Text1.Value;
+            string secondnumber = Text2.Value;
+            int num1 = Convert.ToInt32(firstnumber);
+            int num2 = Convert.ToInt32(secondnumber);
+            Label1.Text = Convert.ToString(num1 * num2);
+        }
+
+        protected void Div_Click(object sender, EventArgs e)
+        {
+            string firstnumber = Text1.Value;
+            string secondnumber = Text2.Value;
+            int num1 = Convert.ToInt32(firstnumber);
+            int num2 = Convert.ToInt32(secondnumber);
+            Label1.Text = Convert.ToString(num1 / num2);
+        }
+
+        protected void Clear_Click(object sender, EventArgs e)
+        {
+            Label1.Text = String.Empty;
+        }
+    }
+}
